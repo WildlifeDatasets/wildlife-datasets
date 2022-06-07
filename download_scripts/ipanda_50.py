a83af28e332c0a9233877a50109f80e85c23278a
cermavo3 <cermavo3@login1.rci.cvut.cz>
2022-06-07 16:10:53 +0200

    ipanda final

--- download_scripts/ipanda_50.py
+++ download_scripts/ipanda_50.py
@@ -3,23 +3,23 @@ import argparse
 import gdown
 import zipfile
 
-parser = argparse.ArgumentParser(description='')
+parser = argparse.ArgumentParser()
 parser.add_argument("--output", type=str, default='../datasets',  help="Output folder")
+parser.add_argument("--name", type=str, default='IPanda-50',  help="Dataset name")
 args = parser.parse_args()
 
-name = 'IPanda-50'
-directory = os.path.join(args.output, name)
+directory = os.path.join(args.output, args.name)
 if not os.path.exists(directory):
     os.makedirs(directory)
 os.chdir(directory)
 
 downloads = [
-    ('iPanda50-images.zip', 'https://drive.google.com/u/0/uc?id=1nkh-g6a8JvWy-XsMaZqrN2AXoPlaXuFg'),
-    ('iPanda50-split.zip', 'https://drive.google.com/uc?id=1gVREtFWkNec4xwqOyKkpuIQIyWU_Y_Ob'),
-    ('iPanda50-eyes-labels.zip', 'https://drive.google.com/uc?id=1jdACN98uOxedZDT-6X3rpbooLAAUEbNY'),
+    ('https://drive.google.com/u/0/uc?id=1nkh-g6a8JvWy-XsMaZqrN2AXoPlaXuFg', 'iPanda50-images.zip'),
+    ('https://drive.google.com/uc?id=1gVREtFWkNec4xwqOyKkpuIQIyWU_Y_Ob', 'iPanda50-split.zip'),
+    ('https://drive.google.com/uc?id=1jdACN98uOxedZDT-6X3rpbooLAAUEbNY', 'iPanda50-eyes-labels.zip'),
 ]
 
-for archive, url in downloads:
+for url, archive in downloads:
     gdown.download(url, archive, quiet=False)
     with zipfile.ZipFile(archive, 'r') as zip_ref:
         zip_ref.extractall('.')
