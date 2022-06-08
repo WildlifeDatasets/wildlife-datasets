83a586c2b2415e5182e482d122d6a9a34712b6ec
cermavo3 <cermavo3@login1.rci.cvut.cz>
2022-06-08 15:47:33 +0200

    add cattle datasets

--- download_scripts/friesian_cattle_2015.py
+++ download_scripts/friesian_cattle_2015.py
@@ -1,5 +1,19 @@
 import os
-url = 'https://data.bris.ac.uk/datasets/wurzq71kfm561ljahbwjhx9n3/wurzq71kfm561ljahbwjhx9n3.zip'
-name = 'FriesianCattle2015'
+import argparse
+import utils
 
-os.system(f"wget -P '../datasets/{name}' {url}")
+parser = argparse.ArgumentParser()
+parser.add_argument("--output", type=str, default='../datasets',  help="Output folder")
+parser.add_argument("--name", type=str, default='FriesianCattle2015',  help="Dataset name")
+args = parser.parse_args()
+
+directory = os.path.join(args.output, args.name)
+if not os.path.exists(directory):
+    os.makedirs(directory)
+os.chdir(directory)
+
+# Download and extract
+url = 'https://data.bris.ac.uk/datasets/wurzq71kfm561ljahbwjhx9n3/wurzq71kfm561ljahbwjhx9n3.zip'
+archive = 'wurzq71kfm561ljahbwjhx9n3.zip'
+utils.download_url(url, archive)
+utils.extract_archive(archive, delete=True)
--- download_scripts/open_cows_2020.py
+++ download_scripts/open_cows_2020.py
@@ -1,5 +1,19 @@
 import os
-url = 'https://data.bris.ac.uk/datasets/tar/10m32xl88x2b61zlkkgz3fml17.zip'
-name = 'OpenCows2020'
+import argparse
+import utils
 
-os.system(f"wget -P '../datasets/{name}' {url}")
+parser = argparse.ArgumentParser()
+parser.add_argument("--output", type=str, default='../datasets',  help="Output folder")
+parser.add_argument("--name", type=str, default='OpenCows2020',  help="Dataset name")
+args = parser.parse_args()
+
+directory = os.path.join(args.output, args.name)
+if not os.path.exists(directory):
+    os.makedirs(directory)
+os.chdir(directory)
+
+# Download and extract
+url = 'https://data.bris.ac.uk/datasets/tar/10m32xl88x2b61zlkkgz3fml17.zip'
+archive = '10m32xl88x2b61zlkkgz3fml17.zip'
+utils.download_url(url, archive)
+utils.extract_archive(archive, delete=True)
