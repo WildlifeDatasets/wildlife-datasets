91d366ab8d5f4ca711698dea35525aaab2355f4d
cermavo3 <cermavo3@login1.rci.cvut.cz>
2022-06-08 14:21:09 +0200

    add nyala and lion datasests

--- download_scripts/lion_data.py
+++ download_scripts/lion_data.py
@@ -1,1 +1,23 @@
-# https://github.com/tvanzyl/wildlife_reidentification
+import os
+import argparse
+import utils
+import shutil
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--output", type=str, default='../datasets',  help="Output folder")
+parser.add_argument("--name", type=str, default='LionData',  help="Dataset name")
+args = parser.parse_args()
+
+directory = os.path.join(args.output, args.name)
+if not os.path.exists(directory):
+    os.makedirs(directory)
+os.chdir(directory)
+
+# Download and extract
+url = 'https://github.com/tvanzyl/wildlife_reidentification/archive/refs/heads/main.zip'
+archive = 'main.zip'
+utils.download_url(url, archive)
+utils.extract_archive(archive, delete=True)
+
+# Cleanup
+shutil.rmtree('wildlife_reidentification-main/Nyala_Data_Zero')
