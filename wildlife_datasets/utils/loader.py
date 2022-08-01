103a608f32926be772ade3300f398b65c3e0d48a
adamluk3 <adamluk3@login1.rci.cvut.cz>
2022-08-01 14:16:23 +0200

    Bugfix

--- wildlife_datasets/utils/loader.py
+++ wildlife_datasets/utils/loader.py
@@ -1,39 +1,41 @@
 import os
 import numpy as np
+import pandas as pd
+from .. import datasets
 
-info_datasets = [
-    (AAUZebraFishID, {}),
-    (AerialCattle2017, {}),
-    (ATRW, {}),
-    (BelugaID, {}),
-    (BirdIndividualID, {'variant': 'source'}),
-    (BirdIndividualID, {'variant': 'segmented'}),
-    (CTai, {}),
-    (CZoo, {}),
-    (Cows2021, {}),
-    (Drosophila, {}),
-    (FriesianCattle2015, {}),
-    (FriesianCattle2017, {}),
-    (GiraffeZebraID, {}),
-    (Giraffes, {}),
-    (HappyWhale, {}),
-    (HumpbackWhaleID, {}),
-    (HyenaID2022, {}),
-    (IPanda50, {}),
-    (LeopardID2022, {}),
-    (LionData, {}),
-    (MacaqueFaces, {}),
-    (NDD20, {}),
-    (NOAARightWhale, {}),
-    (NyalaData, {}),
-    (OpenCows2020, {}),
-    (SealID, {'variant': 'source'}),
-    (SealID, {'variant': 'segmented'}),
-    (SMALST, {}),
-    (StripeSpotter, {}),
-    (WhaleSharkID, {}),
-    (WNIGiraffes, {}),
-    (ZindiTurtleRecall, {})
+info_datasets_full = [
+    (datasets.AAUZebraFishID, {}),
+    (datasets.AerialCattle2017, {}),
+    (datasets.ATRW, {}),
+    (datasets.BelugaID, {}),
+    (datasets.BirdIndividualID, {'variant': 'source'}),
+    (datasets.BirdIndividualID, {'variant': 'segmented'}),
+    (datasets.CTai, {}),
+    (datasets.CZoo, {}),
+    (datasets.Cows2021, {}),
+    (datasets.Drosophila, {}),
+    (datasets.FriesianCattle2015, {}),
+    (datasets.FriesianCattle2017, {}),
+    (datasets.GiraffeZebraID, {}),
+    (datasets.Giraffes, {}),
+    (datasets.HappyWhale, {}),
+    (datasets.HumpbackWhaleID, {}),
+    (datasets.HyenaID2022, {}),
+    (datasets.IPanda50, {}),
+    (datasets.LeopardID2022, {}),
+    (datasets.LionData, {}),
+    (datasets.MacaqueFaces, {}),
+    (datasets.NDD20, {}),
+    (datasets.NOAARightWhale, {}),
+    (datasets.NyalaData, {}),
+    (datasets.OpenCows2020, {}),
+    (datasets.SealID, {'variant': 'source'}),
+    (datasets.SealID, {'variant': 'segmented'}),
+    (datasets.SMALST, {}),
+    (datasets.StripeSpotter, {}),
+    (datasets.WhaleSharkID, {}),
+    (datasets.WNIGiraffes, {}),
+    (datasets.ZindiTurtleRecall, {})
 ]
 
 def unique_datasets_list(datasets_list):
@@ -46,13 +48,28 @@ def unique_datasets_list(datasets_list):
 
     return datasets_list_red
 
-def add_paths(datasets_list, root_dataset, root_dataframe):
-    datasets_list_mod = []
-    for dataset in datasets_list:        
-        if len(dataset[1]) == 0:                        
-            df_path = os.path.join(root_dataframe, dataset[0].__name__ + '.pkl')
+def load_datasets(info_datasets, root_dataset, root_dataframe):
+    # TODO some overwrite would be nice
+    datasets = []
+    for info_dataset in info_datasets:
+        root = os.path.join(root_dataset, info_dataset[0].__name__)
+        if len(info_dataset[1]) == 0:
+            df_path = os.path.join(root_dataframe, info_dataset[0].__name__ + '.pkl')
         else:
-            df_path = os.path.join(root_dataframe, dataset[0].__name__ + '_' + dataset[1]['variant'] + '.pkl')
-        datasets_list_mod.append(dataset + (os.path.join(root_dataset, dataset[0].__name__),) + (df_path,))
-    return datasets_list_mod
+            df_path = os.path.join(root_dataframe, info_dataset[0].__name__ + '_' + info_dataset[1]['variant'] + '.pkl')
+        
+        if os.path.exists(root) and os.path.exists(df_path):
+            df = pd.read_pickle(df_path)
+            dataset = info_dataset[0](root, df, download=False, **info_dataset[1])
+        elif os.path.exists(root) and not os.path.exists(df_path):
+            dataset = info_dataset[0](root, None, download=False, **info_dataset[1])
+            dataset.df.to_pickle(df_path)
+        elif not os.path.exists(root) and os.path.exists(df_path):
+            raise(Exception('Data not found but dataframe found. This should not happen.'))
+        elif not os.path.exists(root) and not os.path.exists(df_path):
+            dataset = info_dataset[0](root, None, download=True, **info_dataset[1])
+            dataset.df.to_pickle(df_path)
+        datasets.append(dataset)
+
+    return datasets
 
