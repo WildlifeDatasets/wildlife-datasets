6e506a0bf094493e0988276195398aa8ccfc5315
adamluk3 <adamluk3@login1.rci.cvut.cz>
2022-10-06 16:49:30 +0200

    Documentation added

--- wildlife_datasets/utils/analysis.py
+++ wildlife_datasets/utils/analysis.py
@@ -1,30 +1,45 @@
 import os
 import numpy as np
+import pandas as pd
 import datetime
 import cv2
+from typing import List
 from matplotlib import pyplot as plt
 from PIL import Image
 from ..datasets import utils
 
-def get_image(path):
-    # We load with OpenCV because PIL does not apply metadata.
+def get_image(path: str) -> Image:
+    '''
+    Loads and image and converts it into PIL.Image.
+    We load it with OpenCV because PIL does not apply metadata.
+    '''    
     img = cv2.imread(path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     return Image.fromarray(img)
 
-def plot_image(img):
+def plot_image(img: Image) -> None:
+    '''
+    Plots an image.
+    '''
     fig, ax = plt.subplots()
     ax.imshow(img)
     plt.show()
 
-def plot_segmentation(img, segmentation):
+def plot_segmentation(img: Image, segmentation: List[float]) -> None:
+    '''
+    Plots an image and its segmentation mask.
+    '''
     if not np.isnan(segmentation).all():
         fig, ax = plt.subplots()
         ax.imshow(img)
         ax.plot(segmentation[0::2], segmentation[1::2], '--', linewidth=5, color='firebrick')
         plt.show()
 
-def plot_bbox_segmentation(df, root, n):
+def plot_bbox_segmentation(df: pd.DataFrame, root: str, n: int) -> None:
+    '''
+    Plots n images from the dataframe df.
+    If bounding boxes or segmnetation masks are present, it plots them as well.
+    '''
     if 'bbox' not in df.columns and 'segmentation' not in df.columns:
         for i in range(n):
             img = get_image(os.path.join(root, df['path'][i]))
@@ -49,52 +64,86 @@ def plot_bbox_segmentation(df, root, n):
                 segmentation = df_red['segmentation'].iloc[i]
                 plot_segmentation(img, segmentation)
 
-def plot_grid(df, root, n_rows=5, n_cols=8, offset=10, img_min=100, rotate=True):
+def plot_grid(
+    df: pd.DataFrame,
+    root: str,
+    n_rows: int = 5,
+    n_cols: int = 8,
+    offset: float = 10,
+    img_min: float = 100,
+    rotate: bool = True
+    ) -> Image:
+    '''
+    Plots a grid of size (n_rows, n_rows) with images from dataframe df.
+    It resizes them based on img_min and possibly rotates if rotate=True.
+    Offset specifies the pixel offset between images.
+    '''
+    # Select indices of images to be plotted
     idx = np.random.permutation(len(df))[:n_rows*n_cols]
 
+    # Load images and compute their ratio
     ratios = []
     for k in idx:
         file_path = os.path.join(root, df['path'][k])
         im = get_image(file_path)
         ratios.append(im.size[0] / im.size[1])
 
+    # Get the size of the images after being resized
     ratio = np.median(ratios)
     if ratio > 1:    
         img_w, img_h = int(img_min*ratio), int(img_min)
     else:
         img_w, img_h = int(img_min), int(img_min/ratio)
 
+    # Create an empty image grid
     im_grid = Image.new('RGB', (n_cols*img_w + (n_cols-1)*offset, n_rows*img_h + (n_rows-1)*offset))
 
+    # Fill the grid image by image
     for i in range(n_rows):
         for j in range(n_cols):
+            # Load the image
             k = n_cols*i + j
             file_path = os.path.join(root, df['path'][idx[k]])
-
             im = get_image(file_path)
+
+            # Possibly rotate the image
             if rotate and ((ratio > 1 and im.size[0] < im.size[1]) or (ratio < 1 and im.size[0] > im.size[1])):
                 im = im.transpose(Image.ROTATE_90)
+
+            # Rescale the image
             im.thumbnail((img_w,img_h))
 
+            # Place the image on the grid
             pos_x = j*img_w + j*offset
             pos_y = i*img_h + i*offset        
             im_grid.paste(im, (pos_x,pos_y))
     return im_grid
 
-def display_statistics(df, root, **kwargs):
+def display_statistics(df):
+    '''
+    Prints statistics about the dataframe df.
+    '''
+    # Remove the unknown identities
     df_red = df.loc[df['identity'] != 'unknown', 'identity']
     df_red.value_counts().reset_index(drop=True).plot()
-        
+    
+    # Compute the total number of identities
     if 'unknown' in list(df['identity'].unique()):
         n_identity = len(df.identity.unique()) - 1
     else:
         n_identity = len(df.identity.unique())
+    
+    # Print general statistics
     print(f"Number of identitites          {n_identity}")
     print(f"Number of all animals          {len(df)}")
     print(f"Number of identified animals   {sum(df['identity'] != 'unknown')}")    
     print(f"Number of unidentified animals {sum(df['identity'] == 'unknown')}")
+
+    # Print statistics about video if present
     if 'video' in df.columns:
         print(f"Number of videos               {len(df[['identity', 'video']].drop_duplicates())}")
+    
+    # Print statistics about time span if present
     if 'date' in df.columns:
         span_years = compute_span(df) / (60*60*24*365.25)
         if span_years > 1:
@@ -104,12 +153,23 @@ def display_statistics(df, root, **kwargs):
         else:
             print(f"Images span                    %1.0f days" % (span_years * 365.25))
 
-def get_dates(dates, frmt):
+def get_dates(dates: pd.Series, frmt: str) -> List[datetime.date]:
+    '''
+    Converts the dates into format frmt.
+    '''
     return np.array([datetime.datetime.strptime(date, frmt) for date in dates])
 
-def compute_span(df):
+def compute_span(df: pd.DataFrame) -> float:
+    '''
+    Compute the time span of the dataset.
+    It is defined as the latest time minus earliest time of image taken.
+    The times are computed separately for each individual.
+    '''
+    # Convert the dates into timedelta
     df = df.loc[~df['date'].isnull()]
     dates = get_dates(df['date'].str[:10], '%Y-%m-%d')
+
+    # Find the maximal span across individuals
     identities = df['identity'].unique()
     span = -np.inf
     for identity in identities:
--- wildlife_datasets/utils/loader.py
+++ wildlife_datasets/utils/loader.py
@@ -1,41 +1,75 @@
 import os
-import numpy as np
 import pandas as pd
+from typing import List
 
-def get_dataset_folder(root_dataset, dataset_class):
+def get_dataset_folder(root_dataset: str, dataset_class) -> pd.DataFrame:
+    '''
+    Gets path to the saved dataset
+    '''
     # TODO: hacky solution
     if dataset_class.__name__.endswith('Segmented'):
         return os.path.join(root_dataset, dataset_class.__name__[:-9])
     else:
         return os.path.join(root_dataset, dataset_class.__name__)
 
-def get_dataframe_path(root_dataframe, dataset_class):
+def get_dataframe_path(root_dataframe: str, dataset_class) -> str:
+    '''
+    Gets path to the pickled dataframe
+    '''
     return os.path.join(root_dataframe, dataset_class.__name__ + '.pkl')
 
-def download_datasets(class_datasets, root_dataset, **kwargs):
+def download_datasets(class_datasets, root_dataset: str, **kwargs) -> None:
+    '''
+    Downloads multiple datasets
+    '''    
     for class_dataset in class_datasets:
         download_dataset(class_dataset, root_dataset, **kwargs)
 
-def download_dataset(class_dataset, root_dataset, overwrite=False):    
+def download_dataset(class_dataset, root_dataset: str, overwrite:bool=False) -> None:
+    '''
+    Downloads one dataset
+    '''
     root = get_dataset_folder(root_dataset, class_dataset)
     if overwrite or not os.path.exists(root):
         class_dataset.download.get_data(root)
         
-def load_datasets(class_datasets, root_dataset, root_dataframe, **kwargs):
+def load_datasets(
+    class_datasets: List[object],
+    root_dataset: str,
+    root_dataframe: str,
+    **kwargs
+    ) -> List[pd.DataFrame]:
+    '''
+    Runs load_dataset for multiple datasets.
+    '''
     return [load_dataset(class_dataset, root_dataset, root_dataframe, **kwargs) for class_dataset in class_datasets]
 
-def load_dataset(class_dataset, root_dataset, root_dataframe, overwrite=False):
+def load_dataset(
+    class_dataset: object,
+    root_dataset: str,
+    root_dataframe: str,
+    overwrite:bool=False
+    ) -> pd.DataFrame:
+    '''
+    Loads the dataframe corresponding to the dataset.
+    If the dataframe was already saved in a pkl file, it loads it.
+    Otherwise, it creates the dataframe and saves it in a pkl file.
+    '''
+    # Check if the dataset is downloaded.
     if not os.path.exists(root_dataset):
         raise(Exception('Data not found. Download them first.'))
-    if not os.path.exists(root_dataframe):
-        os.makedirs(root_dataframe)
-
+    
+    # Get paths of the dataset and the pickled dataframe/
     root = get_dataset_folder(root_dataset, class_dataset)
     df_path = get_dataframe_path(root_dataframe, class_dataset)
     if overwrite or not os.path.exists(df_path):
+        # Create the dataframe, save it and create the dataset
         dataset = class_dataset(root, None, download=False)
+        if not os.path.exists(root_dataframe):
+            os.makedirs(root_dataframe)
         dataset.df.to_pickle(df_path)
     else:
+        # Load the dataframe and create the dataset
         df = pd.read_pickle(df_path)
         dataset = class_dataset(root, df, download=False)
     return dataset
