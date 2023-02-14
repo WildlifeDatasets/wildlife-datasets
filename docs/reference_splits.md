256804ed0bb935eb4f36ea40c1ec295222289f0a
adamluk3 <adamluk3@login1.rci.cvut.cz>
2023-02-14 16:59:55 +0100

    Docs updated

--- docs/reference_splits.md
+++ docs/reference_splits.md
@@ -1,2 +1,24 @@
 # Reference splitting functions
 
+
+<!---
+# TODO: finish
+-->
+
+## Splits
+
+::: splits.BalancedSplit
+    options:
+      heading_level: 3  
+
+## Closed-set split
+
+::: splits.ClosedSetSplit
+    options:
+      heading_level: 3  
+
+## Lcg
+
+::: splits.Lcg
+    options:
+      heading_level: 3  
--- wildlife_datasets/splits/lcg.py
+++ wildlife_datasets/splits/lcg.py
@@ -1,30 +1,49 @@
 import numpy as np
 from typing import List
 
-# TODO: add documentation
+
 class Lcg():
-    '''        
-    Simple Linear congruential generator for generating random numbers.
-    Copied from https://stackoverflow.com/questions/18634079/glibc-rand-function-implementation        
+    """Linear congruential generator for generating random numbers.
+    
+    Copied from [StackOverflow](https://stackoverflow.com/questions/18634079/glibc-rand-function-implementation).
     It is machine-, distribution- and package version-independent.
     It has some drawbacks (check the link above) but perfectly sufficient for our application.
-    '''
+
+    Attributes:
+      state (int): Random state of the LCG.
+    """
+    
     def __init__(self, seed: int, iterate: int=0) -> None:
+        """Initialization function for LCG.
+
+        Args:
+            seed (int): Initial random seed.
+            iterate (int, optional): Number of initial random iterations.
+        """
         self.state = seed
         for _ in range(iterate):
             self.random()
 
     def random(self) -> int:
-        '''
-        Generate random integer from the current state.        
-        '''
+        """Generates a new random integer from the current state.
+
+        Returns:
+            int: New random integer.
+        """
+
         self.state = (self.state * 1103515245 + 12345) & 0x7FFFFFFF
         return self.state
 
-    def random_permutation(self, n: int) -> List[int]:
-        '''
-        Generate random permutation of range(n).
-        '''
+    def random_permutation(self, n: int) -> np.ndarray:
+        """Generates a random permutation of `range(n)`.
+
+        Args:
+            n (int): Length of the sequence to be permuted.
+
+        Returns:
+            np.ndarray: Permuted sequence.
+        """
+        
         rnd = []
         for _ in range(n):
             self.random()
@@ -32,4 +51,13 @@ class Lcg():
         return np.argsort(rnd)
 
     def random_shuffle(self, x: np.ndarray) -> np.ndarray:
+        """Generates a random shuffle of `x`.
+
+        Args:
+            x (np.ndarray): Array to be permuted.
+
+        Returns:
+            np.ndarray: Shuffled array.
+        """
+
         return x[self.random_permutation(len(x))]
