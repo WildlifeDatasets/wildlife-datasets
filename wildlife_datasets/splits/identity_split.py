71ca6c8336655cf56a840b0792982048816cdc78
adamluk3 <adamluk3@login1.rci.cvut.cz>
2023-04-17 12:06:41 +0200

    Id split refactored

--- wildlife_datasets/splits/identity_split.py
+++ wildlife_datasets/splits/identity_split.py
@@ -4,7 +4,91 @@ from typing import List, Tuple
 from .balanced_split import BalancedSplit
 
 
-class ClosedSetSplit(BalancedSplit):
+class IdentitySplit(BalancedSplit):
+    """Base class for `ClosedSetSplit`, `OpenSetSplit` and `DisjointSetSplit`.
+    """
+
+    def modify_df(self, df: pd.DataFrame) -> pd.DataFrame:
+        """Prepares dataframe for splits.
+
+        Removes identities specified in `self.identity_skip` (usually unknown identities).
+
+        Args:
+            df (pd.DataFrame): A dataframe of the data. It must contain columns `identity` and `date`.
+
+        Returns:
+            Modified dataframe of the data.
+        """
+        
+        df = df.copy()
+        df = df[df['identity'] != self.identity_skip]
+        return df
+    
+    def general_split(
+            self,
+            df: pd.DataFrame,
+            individual_train: List[str],
+            individual_test: List[str],
+            ) -> Tuple[np.ndarray, np.ndarray]:
+        """General-purpose split into the training and testing sets.
+
+        It puts all samples of `individual_train` into the training set
+        and all samples of `individual_test` into the testing set.
+        The splitting is performed for each individual separately.
+        The split will result in at least one sample in both the training and testing sets.
+        If only one sample is available for an individual, it will be in the training set.
+                
+        Args:
+            df (pd.DataFrame): A dataframe of the data. It must contain column `identity`.
+            individual_train (List[str]): Individuals to be only in the training test.
+            individual_test (List[str]): Individuals to be only in the testing test.
+
+        Returns:
+            List of labels of the training and testing sets.
+        """
+
+        # Compute how many samples go automatically to the training and testing sets
+        y_counts = df['identity'].value_counts()
+        n_train = sum([y_counts.loc[y] for y in individual_train])
+        n_test = sum([y_counts.loc[y] for y in individual_test])
+        
+        # Recompute ratio_train and adjust it to proper bounds
+        n = len(df)
+        ratio_train = self.ratio_train
+        if n_train + n_test > 0 and n_train + n_test < n:
+            ratio_train = (n*ratio_train - n_train) / (n - n_test - n_train)
+        ratio_train = np.clip(ratio_train, 0, 1)
+        
+        idx_train = np.empty(n, dtype='bool')
+        # Make a loop over all individuals
+        for individual, y_count in y_counts.iteritems():
+            if individual in individual_train and individual in individual_test:
+                # Check if the class does not belong to both sets
+                raise(Exception('Individual cannot be both in individual_train and individual_test.'))
+            elif individual in individual_train:
+                # Check if the class does not belong to the training set
+                idx_train_class = np.ones(y_count, dtype='bool')
+            elif individual in individual_test:
+                # Check if the class does not belong to the testing set
+                idx_train_class = np.zeros(y_count, dtype='bool')
+            else:
+                idx_train_class = np.zeros(y_count, dtype='bool')
+                # Otherwise compute the number of samples in the training set
+                n_train = np.round(ratio_train * y_count).astype(int)
+                if n_train == y_count and n_train > 1:
+                    n_train -= 1
+                if n_train == 0:
+                    n_train = 1
+                # Create indices to the training set and randomly permute them                
+                idx_permutation = self.lcg.random_permutation(y_count)
+                idx_train_class[:n_train] = True                
+                idx_train_class = idx_train_class[idx_permutation]
+            # Save the indices
+            idx_train[df['identity'] == individual] = idx_train_class
+        return np.array(df.index.values)[idx_train], np.array(df.index.values)[~idx_train]
+    
+
+class ClosedSetSplit(IdentitySplit):
     """Closed-set splitting method into training and testing sets.
 
     All individuals are both in the training and testing set.
@@ -40,13 +124,13 @@ class ClosedSetSplit(BalancedSplit):
             List of labels of the training and testing sets.
         """
 
-        df = df[df['identity'] != self.identity_skip]
+        df = self.modify_df(df)
         individual_train = np.array([], dtype=object)
         individual_test = np.array([], dtype=object)
         return self.general_split(df, individual_train, individual_test)
 
 
-class OpenSetSplit(BalancedSplit):
+class OpenSetSplit(IdentitySplit):
     """Open-set splitting method into training and testing sets.
 
     Some individuals are in the testing but not in the training set.
@@ -97,9 +181,8 @@ class OpenSetSplit(BalancedSplit):
             List of labels of the training and testing sets.
         """
         
-        # Remove identities to be skipped
-        df = df[df['identity'] != self.identity_skip]
-        
+        df = self.modify_df(df)
+
         # Compute the counts and randomly permute them
         y_counts = df['identity'].value_counts()
         n_class = len(y_counts)
@@ -120,7 +203,7 @@ class OpenSetSplit(BalancedSplit):
         return self.general_split(df, individual_train, individual_test)
 
 
-class DisjointSetSplit(BalancedSplit):
+class DisjointSetSplit(IdentitySplit):
     """Disjoint-set splitting method into training and testing sets.
 
     No individuals are in both the training and testing sets.
@@ -169,8 +252,7 @@ class DisjointSetSplit(BalancedSplit):
             List of labels of the training and testing sets.
         """
 
-        # Remove identities to be skipped
-        df = df[df['identity'] != self.identity_skip]
+        df = self.modify_df(df)
         
         # Compute the counts and randomly permute them
         y_counts = df['identity'].value_counts()
