e4fbf76fe706841dfa6bc3d533b505fb3cee6bc9
adamluk3 <adamluk3@login1.rci.cvut.cz>
2023-05-15 13:36:13 +0200

    Unknown class renamed

--- wildlife_datasets/metrics/metrics.py
+++ wildlife_datasets/metrics/metrics.py
@@ -4,41 +4,41 @@ import sklearn.metrics as skm
 # TODO: add documentation
 # TODO: check all code
 
-def unify_types(y_true, y_pred, unknown_class):
-    y_all = list(set(y_true).union(set(y_pred)) - set([unknown_class]))        
+def unify_types(y_true, y_pred, new_class):
+    y_all = list(set(y_true).union(set(y_pred)) - set([new_class]))        
     y_types = set([type(y) for y in y_all])
     if len(y_types) > 1:
         raise(Exception('Labels have mixed types. Convert all to int or str.'))
-    if str in y_types and isinstance(unknown_class, int):
-        encoder = {unknown_class: unknown_class}
+    if str in y_types and isinstance(new_class, int):
+        encoder = {new_class: new_class}
         for i, y in enumerate(y_all):
-            encoder[y] = unknown_class + 1 + i
+            encoder[y] = new_class + 1 + i
         y_true = [encoder[y] for y in y_true]
         y_pred = [encoder[y] for y in y_pred]
-    if int in y_types and isinstance(unknown_class, str):
-        encoder = {unknown_class: np.min(y_all) - 1}
+    if int in y_types and isinstance(new_class, str):
+        encoder = {new_class: np.min(y_all) - 1}
         for i, y in enumerate(y_all):
             encoder[y] = y
         y_true = [encoder[y] for y in y_true]
         y_pred = [encoder[y] for y in y_pred]
-        unknown_class = encoder[unknown_class]
-    return y_true, y_pred, unknown_class
+        new_class = encoder[new_class]
+    return y_true, y_pred, new_class
 
 
 def accuracy(
         y_true,
         y_pred,
-        unknown_class=None,
+        new_class=None,
     ):
-    y_true, y_pred, unknown_class = unify_types(y_true, y_pred, unknown_class)
+    y_true, y_pred, new_class = unify_types(y_true, y_pred, new_class)
     return np.mean(np.array(y_pred) == np.array(y_true))
 
 def balanced_accuracy(
         y_true,
         y_pred,
-        unknown_class=None,
+        new_class=None,
     ):
-    y_true, y_pred, unknown_class = unify_types(y_true, y_pred, unknown_class)    
+    y_true, y_pred, new_class = unify_types(y_true, y_pred, new_class)    
     C = skm.confusion_matrix(y_true, y_pred)
     with np.errstate(divide="ignore", invalid="ignore"):
         per_class = np.diag(C) / C.sum(axis=1)
@@ -47,27 +47,27 @@ def balanced_accuracy(
 def class_average_accuracy(
         y_true,
         y_pred,
-        unknown_class=None,
+        new_class=None,
     ):
-    y_true, y_pred, unknown_class = unify_types(y_true, y_pred, unknown_class)
+    y_true, y_pred, new_class = unify_types(y_true, y_pred, new_class)
     C = skm.multilabel_confusion_matrix(y_true, y_pred)
     return np.mean([C_i[0,0]+C_i[1,1] for C_i in C]) / np.sum(C[0])
 
 def precision(
         y_true,
         y_pred,
-        unknown_class=None,
+        new_class=None,
     ):
-    y_true, y_pred, unknown_class = unify_types(y_true, y_pred, unknown_class)
+    y_true, y_pred, new_class = unify_types(y_true, y_pred, new_class)
     return skm.precision_score(y_true, y_pred, average='macro')
 
 def recall(
         y_true,
         y_pred,
-        unknown_class=None,
+        new_class=None,
         ignore_empty=False
     ):
-    y_true, y_pred, unknown_class = unify_types(y_true, y_pred, unknown_class)
+    y_true, y_pred, new_class = unify_types(y_true, y_pred, new_class)
     if ignore_empty:
         C = skm.multilabel_confusion_matrix(y_true, y_pred)
         return np.mean([C_i[1,1]/(C_i[1,0]+C_i[1,1]) for C_i in C if C_i[1,0]+C_i[1,1] > 0])
@@ -77,20 +77,20 @@ def recall(
 def f1(
         y_true,
         y_pred,
-        unknown_class=None,
+        new_class=None,
     ):
-    y_true, y_pred, unknown_class = unify_types(y_true, y_pred, unknown_class)
+    y_true, y_pred, new_class = unify_types(y_true, y_pred, new_class)
     return skm.f1_score(y_true, y_pred, average='macro')
 
 def accuracy_known_samples(
         y_true,
         y_pred,
-        unknown_class,
+        new_class,
     ):
-    y_true, y_pred, unknown_class = unify_types(y_true, y_pred, unknown_class)
+    y_true, y_pred, new_class = unify_types(y_true, y_pred, new_class)
     y_true = np.array(y_true)    
     y_pred = np.array(y_pred)
-    known = y_true != unknown_class
+    known = y_true != new_class
     if sum(known) > 0:
         return np.mean(y_true[known] == y_pred[known])
     else:
@@ -99,12 +99,12 @@ def accuracy_known_samples(
 def accuracy_unknown_samples(
         y_true,
         y_pred,
-        unknown_class,
+        new_class,
     ):
-    y_true, y_pred, unknown_class = unify_types(y_true, y_pred, unknown_class)
+    y_true, y_pred, new_class = unify_types(y_true, y_pred, new_class)
     y_true = np.array(y_true)    
     y_pred = np.array(y_pred)
-    unknown = y_true == unknown_class
+    unknown = y_true == new_class
     if sum(unknown) > 0:
         return np.mean(y_true[unknown] == y_pred[unknown])
     else:
@@ -113,11 +113,11 @@ def accuracy_unknown_samples(
 def normalized_accuracy(
         y_true,
         y_pred,
-        unknown_class,
+        new_class,
         mu
     ):
-    aks = accuracy_known_samples(y_true, y_pred, unknown_class)
-    aus = accuracy_unknown_samples(y_true, y_pred, unknown_class)
+    aks = accuracy_known_samples(y_true, y_pred, new_class)
+    aus = accuracy_unknown_samples(y_true, y_pred, new_class)
     return mu*aks + (1-mu)*aus
 
 def average_precision(
@@ -143,11 +143,11 @@ def mean_average_precision(
 def auc_roc_new_class(
         y_true,
         y_score,
-        unknown_class,
+        new_class,
     ):
     # TODO: check if it is correct
     # TODO: low y_score means high chance of new individual
-    y_true, _, unknown_class = unify_types(y_true, [], unknown_class)    
-    a = np.array(y_true) == unknown_class
+    y_true, _, new_class = unify_types(y_true, [], new_class)    
+    a = np.array(y_true) == new_class
     b = -np.array(y_score)
     return skm.roc_auc_score(a, b)
