12d814ba82c745dc18c62a98b65f6492777e7205
sadda <lukas.adam.cr@gmail.com>
2024-01-29 09:24:41 +0100

    plot_grid refactor

--- wildlife_datasets/analysis/plots.py
+++ wildlife_datasets/analysis/plots.py
@@ -46,12 +46,12 @@ def plot_bbox_segmentation(df: pd.DataFrame, root: str, n: int) -> None:
 
     if 'bbox' not in df.columns and 'segmentation' not in df.columns:
         for i in range(min(n, len(df))):
-            img = get_image(os.path.join(root, df['path'].iloc[i]))
+            img = utils.get_image(os.path.join(root, df['path'].iloc[i]))
             plot_image(img)
     if 'bbox' in df.columns:
         df_red = df[~df['bbox'].isnull()]
         for i in range(min(n, len(df_red))):
-            img = get_image(os.path.join(root, df_red['path'].iloc[i]))
+            img = utils.get_image(os.path.join(root, df_red['path'].iloc[i]))
             segmentation = utils.bbox_segmentation(df_red['bbox'].iloc[i])
             plot_segmentation(img, segmentation)
     if 'segmentation' in df.columns:
@@ -59,12 +59,12 @@ def plot_bbox_segmentation(df: pd.DataFrame, root: str, n: int) -> None:
         for i in range(min(n, len(df_red))):
             segmentation = df_red['segmentation'].iloc[i]
             if type(segmentation) == str:
-                img = get_image(os.path.join(root, df_red['path'].iloc[i]))
+                img = utils.get_image(os.path.join(root, df_red['path'].iloc[i]))
                 plot_image(img)
-                img = get_image(os.path.join(root, segmentation))
+                img = utils.get_image(os.path.join(root, segmentation))
                 plot_image(img)
             elif type(segmentation) == list or type(segmentation) == np.ndarray:
-                img = get_image(os.path.join(root, df_red['path'].iloc[i]))
+                img = utils.get_image(os.path.join(root, df_red['path'].iloc[i]))
                 segmentation = df_red['segmentation'].iloc[i]
                 plot_segmentation(img, segmentation)
 
