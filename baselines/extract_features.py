3873f23d33b659f809da145231de39e2b1cd6226
sadda <lukas.adam.cr@gmail.com>
2024-05-20 07:31:30 +0000

    Path changed

--- baselines/extract_features.py
+++ baselines/extract_features.py
@@ -6,13 +6,13 @@ from wildlife_tools.features import DeepFeatures
 from wildlife_tools.data import WildlifeDataset
 
 device = 'cuda'
-root = 'data/WildlifeReID-10k'
+root = '/data/wildlife_datasets/data/WildlifeReID-10k'
 d = datasets.WildlifeReID10k(root)
 
 # Extract features by MegaDescriptor
 model = create_model("hf-hub:BVRA/MegaDescriptor-L-384", pretrained=True)
 model = model.eval()
-extractor = DeepFeatures(model, device=device, batch_size=64)
+extractor = DeepFeatures(model, device=device, batch_size=32)
 transform = T.Compose([
     T.Resize([384, 384]),
     T.ToTensor(),
@@ -24,7 +24,7 @@ np.save('features_mega.npy', features)
 
 # Extract features by Dinov2
 model = create_model("hf-hub:timm/vit_large_patch14_dinov2.lvd142m", pretrained=True).to('cuda')
-extractor = DeepFeatures(model, device='cuda', batch_size=64)
+extractor = DeepFeatures(model, device='cuda', batch_size=32)
 transform = T.Compose([
     T.Resize(size=518),
     T.CenterCrop(size=[518, 518]),
