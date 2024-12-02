62173a72be9b087768accf0fe3b32ea986e42d0b
sadda <lukas.adam.cr@gmail.com>
2024-12-02 10:46:56 +0000

    Extraction dataset-wise

--- baselines/extract_features.py
+++ baselines/extract_features.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import torchvision.transforms as T
 from timm import create_model
@@ -7,7 +8,9 @@ from wildlife_tools.data import WildlifeDataset
 
 device = 'cuda'
 root = '/data/wildlife_datasets/data/WildlifeReID10k'
+root_output = 'features'
 d = datasets.WildlifeReID10k(root)
+os.makedirs(root_output, exist_ok=True)
 
 # Extract features by Dinov2
 model = create_model("hf-hub:timm/vit_large_patch14_dinov2.lvd142m", pretrained=True).to(device)
@@ -18,7 +21,8 @@ transform = T.Compose([
     T.ToTensor(),
     T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
 ])
-dataset = WildlifeDataset(metadata=d.df, root=d.root, transform=transform)
-features = extractor(dataset)
-np.save('features_dino.npy', features)
-np.save('features_dino_names.npy', dataset.metadata['path'])
+for name, df_dataset in d.df.groupby('dataset'):
+    dataset = WildlifeDataset(metadata=df_dataset, root=d.root, transform=transform)
+    features = extractor(dataset)
+    np.save(os.path.join(root_output, f'features_{name}.npy'), features)
+    np.save(os.path.join(root_output, f'names_{name}.npy'), dataset.metadata['path'])
