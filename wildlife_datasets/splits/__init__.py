642217e00b5347151e1ee640280e134f882dadfc
sadda <lukas.adam.cr@gmail.com>
2025-02-12 15:52:00 +0000

    Better export

--- wildlife_datasets/splits/__init__.py
+++ wildlife_datasets/splits/__init__.py
@@ -1,5 +1,5 @@
 from .analysis import analyze_split, extract_data_split, recognize_id_split, recognize_time_split, visualize_split
-from .balanced_split import *
-from .identity_split import *
+from .balanced_split import BalancedSplit
+from .identity_split import FullSplit, IdentitySplit, ClosedSetSplit, OpenSetSplit, DisjointSetSplit
 from .lcg import Lcg
-from .time_aware_split import *
+from .time_aware_split import TimeAwareSplit, TimeProportionSplit, TimeCutoffSplit, TimeCutoffSplitAll, RandomProportion
